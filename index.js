f1594c9ef25382a37ca24d558bd5ecdd63cc4a3a
Justin Li <justinlit23@gmail.com>
2022-04-04 10:09:43 -0700

    commit tree 16e3abcb221909be61c8d41fe8ec55a3cde2a88a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Penelope Pepler';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(welcomeMessage);
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting);
